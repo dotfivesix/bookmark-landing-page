0c8f707fecb9fadc4e54fc8e430fe3056a557fa5
DarkDev <mda.business.on@gmail.com>
2023-01-10 12:45:47 +0500

    Setup File Structure and TailwindCSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"soft-blue" : "hsl(231, 69%, 60%)",
+				"soft-red" : "hsl(0, 94%, 66%)",
+				"grayish-blue" : "hsl(229, 8%, 60%)",
+				"very-dark-blue" : "hsl(229, 31%, 21%)"
+			},
+			fontFamily: {
+				"rubik": "'Rubik', sans-serif"
+			}
+		},
 	},
 	plugins: [],
 }
